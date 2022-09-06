54027abdcf810d3b7121c500c6c9b0689b0237a3
User <User@ALEXDATOR>
2022-09-06 12:58:03 +0200

    Add project files.

---
+++ Tapet/Program.cs
@@ -0,0 +1,22 @@
+﻿float vägghöjd = 0;
+float väggbredd = 0;
+
+
+float tapetlängd = 10.05f;
+float tapetbredd = 0.53f;
+
+float tapetrulle = tapetlängd * tapetbredd;
+
+Console.WriteLine("-------------------Forza Juve------------------------------------------");
+Console.WriteLine("Skriv in hur hög väggen är i meter");
+vägghöjd = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Skriv in hur bred väggen är i meter");
+väggbredd = float.Parse(Console.ReadLine());
+
+float väggstorlek = vägghöjd * väggbredd;
+//float tapetremsor = tapetlängd / vägghöjd;
+
+Console.WriteLine("Du behöver " + väggstorlek / tapetrulle + " tapetrullar");
+
+Console.WriteLine("--------------------Juve Forever-----------------------------------------");
